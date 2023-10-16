dcd880771f46cd681b198b0fb17e1d27e696cef8
unknown <ekenehanson@gmail.com.com>
2023-10-16 11:47:56 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+operation = """
+Please select an operation
+A - for Addition
+S  - for Subtraction
+M  - for Multiplication
+D  - for Division
+P  - for Power
+R  - for Modulus
+Q - for Sqaure Root
+Z - for Square: 
+"""
+first_name = input("Please enter the first number: ")
+second_name = input("Please enter the second number: ")
+if operation =="A":
+    print(first_name + second_name)
